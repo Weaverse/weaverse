48a256043f1c150175537e27a1edb510eb3e440c
Paul <phanminh65@gmail.com>
2021-11-16 23:58:58 +0700

    updates

--- packages/sdk-react/src/index.tsx
+++ packages/sdk-react/src/index.tsx
@@ -1,10 +1,18 @@
-import * as React from 'react';
-import {useEffect} from "react";
+import * as React from 'react'
+import {useEffect} from 'react'
 
 // Delete me
 export const Thing = () => {
+  let [color, setColor] = React.useState('red')
   useEffect(() => {
-    console.log('Thing');
+    window.addEventListener('message', (event) => {
+      console.log(event.data)
+      if (event.data.type.startsWith('HW')) {
+        if (event.data.type === 'HW_SET_COLOR') {
+          setColor(event.data.payload)
+        }
+      }
+    })
   }, []);
-  return <div>the snozzberries taste like 11111</div>;
+  return <div style={{color}}>the snozzberries taste like 11111</div>
 };
