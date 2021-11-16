507f22afa125632b727a0eb3e9006c7f3b12210c
Paul <phanminh65@gmail.com>
2021-11-16 22:55:39 +0700

    first commit

---
+++ packages/sdk-react/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {useEffect} from "react";
+
+// Delete me
+export const Thing = () => {
+  useEffect(() => {
+    console.log('Thing');
+  }, []);
+  return <div>the snozzberries taste like 11111</div>;
+};
