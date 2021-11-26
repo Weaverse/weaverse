e547a45ab4e85ff4dc5ded44037855251a464c34
Paul <phanminh65@gmail.com>
2021-11-26 18:17:03 +0700

    updates

--- packages/sdk-react/src/index.tsx
+++ packages/sdk-react/src/index.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react'
 import {useEffect} from 'react'
+import {css} from './theme'
 
-// Delete me
+
 export const Thing = () => {
   let [color, setColor] = React.useState('red')
   useEffect(() => {
     window.addEventListener('message', (event) => {
       console.log(event.data)
-      if (event.data.type.startsWith('HW')) {
+      if (event.data.type?.startsWith('HW')) {
         if (event.data.type === 'HW_SET_COLOR') {
           setColor(event.data.payload)
         }
       }
     })
-  }, []);
-  return <div style={{color}}>the snozzberries taste like 11111</div>
+  }, [])
+  let cssClass = css({
+    backgroundColor: color,
+    borderRadius: '9999px',
+    fontSize: '13px',
+    padding: '10px 15px'
+  })()
+  return <button className={cssClass}>Test Element</button>
 };
