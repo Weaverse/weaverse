01fad6931983763e01c5c1e60012d69a5348e8d5
Paul <phanminh65@gmail.com>
2022-01-07 11:22:30 +0700

    add grid element

---
+++ packages/react/src/elements/grid.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Grid(props: any) {
+  return <div {...props} />
+}
+
+Grid.defaultProps = {
+  style: {
+    // display: 'grid',
+    // outline: '1px solid red',
+  },
+  type: 'grid'
+}
+
+Grid.configs = {
+  type: 'grid'
+}
