0aa8e86a2c356fd0462d9f60d6e52c97dfb08ad8
Paul <phanminh65@gmail.com>
2022-03-03 22:50:49 +0700

    update: deployment setting

--- apps/remix/app/weaverse-content.tsx
+++ apps/remix/app/weaverse-content.tsx
@@ -3,8 +3,9 @@ import {useEffect} from 'react'
 
 export let weaverseContext = createRootContext({projectKey: '1'})
 
-export const WeaverseContent = ({defaultData}) => {
+export const WeaverseContent = ({defaultData}: any) => {
   useEffect(() => {
+    // @ts-ignore
     window.weaverseContext = weaverseContext
   }, [])
   return <WeaverseRoot context={weaverseContext} defaultData={defaultData}/>
