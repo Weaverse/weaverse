d4894c7577bd029f3bcb72ba57365650efff028c
Paul <phanminh65@gmail.com>
2022-03-07 18:20:22 +0700

    feat: updating element mechanism

--- apps/next/pages/[[...all]].js
+++ apps/next/pages/[[...all]].js
@@ -1,9 +1,16 @@
 import WeaverseContent, {weaverseContext} from '../weaverse-content'
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   let data = await weaverseContext.fetchProjectData()
   return {
     props: data // will be passed to the page component as props
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true
   }
 }
 
