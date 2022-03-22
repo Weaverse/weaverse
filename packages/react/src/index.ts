8ef9eea3c90d5639f01ec232d3611bef6f31aa6c
Paul <phanminh65@gmail.com>
2022-03-22 17:53:52 +0700

    initializing shopify sdk

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,7 @@
-export {WeaverseRoot, createRootContext} from './renderer'
-
-
+export { WeaverseRoot, createRootContext } from './renderer'
+export {
+  isBrowser,
+  Weaverse,
+  WeaverseItemStore,
+  WeaverseType,
+} from '@weaverse/core'
---
+++ packages/shopify/src/index.ts
@@ -0,0 +1,5 @@
+import { WeaverseRoot, createRootContext } from '@weaverse/react'
+
+let something = () => {}
+
+export { WeaverseRoot, createRootContext, something }
