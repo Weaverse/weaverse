04d5ed65889306f59b9c178aeac1143b6a3456de
Paul <phanminh65@gmail.com>
2022-03-26 00:19:53 +0700

    feat: prototyping binding mechanism with text element

--- packages/react/src/elements/text.tsx
+++ packages/react/src/elements/text.tsx
@@ -1,16 +1,27 @@
 import * as React from 'react'
 
 export const Text = React.forwardRef((props: any, ref) => {
-  let {children, ...rest} = props
-  return <span ref={ref} {...rest} contentEditable={true} dangerouslySetInnerHTML={{__html: '123123'}}/>
+  let {children, value, ...rest} = props
+  return <span ref={ref} {...rest} contentEditable={true} dangerouslySetInnerHTML={{__html: value}}/>
 })
 
 Text.defaultProps = {
-  style: {},
+  value: 'sample text',
   tag: 'span'
 }
 
 export let schema = {
   type: 'text',
+  inspectors: [{
+    binding: 'data',
+    key: 'value',
+    componentType: 'textarea'
+  },
+    {
+      binding: 'style',
+      key: 'color',
+      componentType: 'color'
+    }
+  ]
 }
 export default Text
