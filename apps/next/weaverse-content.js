9accb001f82a9304900c6edd761ee9b603115bfe
Paul <phanminh65@gmail.com>
2022-03-27 11:18:28 +0700

    chore: remove apps package to move it to separate repo

--- apps/next/weaverse-content.js
+++
@@ -1,9 +0,0 @@
-import {createRootContext, WeaverseRoot} from '@weaverse/react'
-
-export let weaverseContext = createRootContext({projectKey: '1'})
-
-export const WeaverseContent = ({defaultData}) => {
-	return <WeaverseRoot context={weaverseContext} defaultData={defaultData}/>
-}
-
-export default WeaverseContent
