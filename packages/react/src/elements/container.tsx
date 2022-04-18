3cdcded708d70f13d27d281841cfb02ce58bc692
Paul <phanminh65@gmail.com>
2022-04-18 18:04:52 +0700

    feat: update text element, tinymce text editor

--- packages/react/src/elements/container.tsx
+++ packages/react/src/elements/container.tsx
@@ -13,6 +13,10 @@ export let schema: WeaverseElementSchema = {
       '@desktop': {
         display: 'flex',
         backgroundColor: 'rgba(248,203,203,0.38)',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        textAlign: 'left',
         gridArea: '1 / 1 / 3 / 6',
         minHeight: '96px',
         overflow: 'hidden',
