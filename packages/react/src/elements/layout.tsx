3510b8e54ab86f87d0c8d02ad2e54c8d02525f1b
Leo Huynh <huynhtuananh21895@gmail.com>
2022-05-19 23:04:14 +0700

    feat: update layout element

--- packages/react/src/elements/layout.tsx
+++ packages/react/src/elements/layout.tsx
@@ -2,12 +2,25 @@ import { WeaverseElementSchema } from '@weaverse/core'
 import React, { forwardRef } from 'react'
 
 let Layout = forwardRef((props: any, ref) => {
+  const { css, children, rowCount, rowGap, rowHeight, columnCount, columnGap, gridWidth, layoutWidth, ...rest } = props
   return (
     <div
       ref={ref}
-      {...props}
-      children={props.children}
-    />
+      style={{
+        '--row-count': rowCount,
+        '--row-gap': rowGap + 'px',
+        '--row-height': rowHeight + 'px',
+        '--col-count': columnCount,
+        '--col-gap': columnGap + 'px',
+        '--grid-width': gridWidth + 'px',
+        '--layout-width': layoutWidth + '%',
+        '--edge-width': 'calc((var(--layout-width) - var(--grid-width)) / 2)',
+        '--col-width': 'calc((var(--grid-width) - calc(var(--col-count) - 1) * var(--col-gap)) / var(--col-count))'
+      }}
+      {...rest}
+    >
+      {children}
+    </div>
   )
 })
 
@@ -18,20 +31,22 @@ export let schema: WeaverseElementSchema = {
     css: {
       '@desktop': {
         display: 'grid !important',
-        gridTemplateRows: 'repeat($row-count, $row-size)',
-        gridTemplateColumns: 'repeat($column-count, 1fr)',
-        // gridAutoRows: '$sizes$rowSize',
-        gridGap: '$gap',
-        minHeight: '104px',
-        width: '$width',
-        maxWidth: '$max-width',
-        margin: '0 auto',
+        gridTemplateRows: 'repeat(var(--row-count), var(--row-height))',
+        gridTemplateColumns: 'var(--edge-width) 1fr repeat(var(--col-count),var(--col-width)) 1fr var(--edge-width)',
+        gridGap: 'var(--row-gap) var(--col-gap)',
       },
       '@mobile': {
         display: 'flex !important',
         flexDirection: 'column',
       },
     },
+    rowCount: 12,
+    rowGap: 8,
+    rowHeight: 48,
+    columnCount: 16,
+    columnGap: 8,
+    gridWidth: 1224,
+    layoutWidth: 100,
   },
   settings: [
   ],
