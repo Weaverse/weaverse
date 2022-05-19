dfb14bac0a639f5d291c2185509ddb69f96dd159
Paul <phanminh65@gmail.com>
2022-05-19 18:07:48 +0700

    feat: product element mechanism

--- packages/shopify/src/elements/product/context.ts
+++ packages/shopify/src/elements/product/context.ts
@@ -3,6 +3,12 @@ import {ProductContextProps} from '../../type'
 export let ProductContext = createContext<ProductContextProps>({})
 export let ProductProvider = ProductContext.Provider
 export let ProductConsumer = ProductContext.Consumer
-
+// @ts-ignore
+export let weaverseShopifyProducts = new Proxy(globalThis?.weaverseShopifyProducts || {}, {
+  get: (target, name) => {
+    // @ts-ignore
+    return target?.[name] || globalThis?.weaverseShopifyProducts?.[name]
+  },
+})
 
 // export let ProductListContext = createContext<ProductContextProps>({})
