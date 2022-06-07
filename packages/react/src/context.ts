bebcc1de1c0c84ec547173b735efdfe248a6cffc
Leo Huynh <huynhtuananh21895@gmail.com>
2022-06-07 16:53:29 +0700

    feat: add ctx for react pkg

---
+++ packages/react/src/context.ts
@@ -0,0 +1,16 @@
+import { Weaverse, WeaverseType } from '@weaverse/core'
+import { createContext } from 'react'
+import Elements from './elements'
+
+export let createRootContext = (configs: WeaverseType) => {
+  let rootContext = new Weaverse(configs)
+  // Register the element components
+  Object.keys(Elements).forEach((key) => {
+    rootContext.registerElement(Elements[key])
+  })
+  return rootContext
+}
+
+export let WeaverseContext = createContext<Weaverse>({} as Weaverse)
+export let WeaverseContextProvider = WeaverseContext.Provider
+export let WeaverseContextConsumer = WeaverseContext.Consumer
