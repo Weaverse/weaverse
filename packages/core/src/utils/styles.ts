84c9d9d3743ee5e6b8a92553643ecec8463f64fd
Leo Huynh <huynhtuananh21895@gmail.com>
2022-07-18 14:54:29 +0700

    chore(types): add stitches utils types

--- packages/core/src/utils/styles.ts
+++ packages/core/src/utils/styles.ts
@@ -1,40 +1,40 @@
 export const stichesUtils = {
   // Abbreviated margin properties
-  m: (value) => ({
+  m: (value: string) => ({
     margin: value
   }),
-  mt: (value) => ({
+  mt: (value: string) => ({
     marginTop: value
   }),
-  mr: (value) => ({
+  mr: (value: string) => ({
     marginRight: value
   }),
-  mb: (value) => ({
+  mb: (value: string) => ({
     marginBottom: value
   }),
-  ml: (value) => ({
+  ml: (value: string) => ({
     marginLeft: value
   }),
-  mx: (value) => ({
+  mx: (value: string) => ({
     marginLeft: value,
     marginRight: value
   }),
-  my: (value) => ({
+  my: (value: string) => ({
     marginTop: value,
     marginBottom: value
   }),
 
   // A property for applying width/height together
-  size: (value) => ({
+  size: (value: string) => ({
     width: value,
     height: value
   }),
   // Abbreviated padding properties
-  px: (value) => ({
+  px: (value: string) => ({
     paddingLeft: value,
     paddingRight: value
   }),
-  py: (value) => ({
+  py: (value: string) => ({
     paddingTop: value,
     paddingBottom: value
   })
