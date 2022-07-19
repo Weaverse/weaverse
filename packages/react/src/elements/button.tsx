9e66a3e92c5f0c59d01a90b6a00176cad0ef90ce
Leo Huynh <huynhtuananh21895@gmail.com>
2022-07-19 17:05:17 +0700

    chore: remove Button's schema

--- packages/react/src/elements/button.tsx
+++ packages/react/src/elements/button.tsx
@@ -6,7 +6,11 @@ export const Button = React.forwardRef((props: any, ref: any) => {
 
   if (target) {
     return (
-      <a href={target} target={openInNewTab ? '_blank' : '_self'}>
+      <a
+        href={target}
+        target={openInNewTab ? '_blank' : '_self'}
+        rel="noreferrer"
+      >
         <button ref={ref} {...rest}>
           {props.value}
         </button>
@@ -25,59 +29,59 @@ Button.defaultProps = {
   value: 'Shop now',
 }
 
-export const schema: WeaverseElementSchema = {
-  title: 'Button',
-  type: 'button',
-  parentType: 'container',
-  styles: [
-    {
-      type: 'dimensions',
-    },
-    {
-      type: 'alignment',
-    },
-    {
-      type: 'border',
-    },
-    {
-      type: 'background',
-    },
-    {
-      type: 'spacing',
-    },
-  ],
-  settings: [],
-  toolbar: [
-    {
-      type: 'delete',
-    },
-    {
-      type: 'duplicate',
-    },
-    {
-      type: 'link',
-    },
-    {
-      type: 'color',
-    },
-  ],
-  data: {
-    css: {
-      '@desktop': {
-        borderRadius: '72px',
-        border: 'none',
-        backgroundColor: '#0F71FF',
-        color: '#fff',
-        fontSize: '13px',
-        padding: '10px 20px',
-      },
-    },
-    target: '',
-    openInNewTab: false,
-  },
-  flags: {
-    draggable: true,
-  },
-}
+// export const schema: WeaverseElementSchema = {
+//   title: 'Button',
+//   type: 'button',
+//   parentType: 'container',
+//   styles: [
+//     {
+//       type: 'dimensions',
+//     },
+//     {
+//       type: 'alignment',
+//     },
+//     {
+//       type: 'border',
+//     },
+//     {
+//       type: 'background',
+//     },
+//     {
+//       type: 'spacing',
+//     },
+//   ],
+//   settings: [],
+//   toolbar: [
+//     {
+//       type: 'delete',
+//     },
+//     {
+//       type: 'duplicate',
+//     },
+//     {
+//       type: 'link',
+//     },
+//     {
+//       type: 'color',
+//     },
+//   ],
+//   data: {
+//     css: {
+//       '@desktop': {
+//         borderRadius: '72px',
+//         border: 'none',
+//         backgroundColor: '#0F71FF',
+//         color: '#fff',
+//         fontSize: '13px',
+//         padding: '10px 20px',
+//       },
+//     },
+//     target: '',
+//     openInNewTab: false,
+//   },
+//   flags: {
+//     draggable: true,
+//   },
+// }
 
 export default Button
