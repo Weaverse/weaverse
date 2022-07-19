2bfef5c5236a4ec66bff8c2aede52dc74eeefce0
Leo Huynh <huynhtuananh21895@gmail.com>
2022-07-19 18:24:59 +0700

    chore: update button

--- packages/react/src/elements/button.tsx
+++ packages/react/src/elements/button.tsx
@@ -2,7 +2,7 @@ import { WeaverseElementSchema } from '@weaverse/core'
 import * as React from 'react'
 
 export const Button = React.forwardRef((props: any, ref: any) => {
-  const { buttonType, css, openInNewTab = false, target = '', ...rest } = props
+  const { openInNewTab, target, value, ...rest } = props
 
   if (target) {
     return (
@@ -10,23 +10,36 @@ export const Button = React.forwardRef((props: any, ref: any) => {
         href={target}
         target={openInNewTab ? '_blank' : '_self'}
         rel="noreferrer"
+        {...rest}
+        ref={ref}
       >
-        <button ref={ref} {...rest}>
-          {props.value}
-        </button>
+        {value}
       </a>
     )
   }
 
   return (
     <button ref={ref} {...rest}>
-      {props.value}
+      {value}
     </button>
   )
 })
 
 Button.defaultProps = {
   value: 'Shop now',
+  openInNewTab: false,
+  target: '',
+  type: 'button',
+  css: {
+    '@desktop': {
+      borderRadius: '72px',
+      border: 'none',
+      backgroundColor: '#0F71FF',
+      color: '#fff',
+      fontSize: '13px',
+      padding: '10px 20px',
+    },
+  },
 }
 
 // export const schema: WeaverseElementSchema = {
