8e5488e66ce9b8b201f6da1e8ad12e21997ba659
Leo Huynh <huynhtuananh21895@gmail.com>
2022-07-19 17:27:52 +0700

    fix: missing rest prop on Countdown

--- packages/react/src/elements/countdown/index.tsx
+++ packages/react/src/elements/countdown/index.tsx
@@ -4,7 +4,7 @@ import { WeaverseContext } from '../../context'
 import { TimerBlock } from './timer-block'
 import { getTime, times } from './utils'
 
-const Countdown = forwardRef<HTMLElement, TODO>((props, ref) => {
+const Countdown = forwardRef<HTMLDivElement, TODO>((props, ref) => {
   const { isDesignMode } = useContext(WeaverseContext)
   let {
     type,
@@ -15,6 +15,7 @@ const Countdown = forwardRef<HTMLElement, TODO>((props, ref) => {
     openInNewTab,
     showColon,
     showLabel,
+    ...rest
   } = props
 
   const [remaining, setRemaining] = React.useState(0)
@@ -47,17 +48,15 @@ const Countdown = forwardRef<HTMLElement, TODO>((props, ref) => {
 
   const timer = getTime(remaining)
   return (
-    <div ref={ref} {...props}>
+    <div ref={ref} {...rest}>
       {times.map((time) => {
         return (
-          <>
-            <TimerBlock
-              key={time}
-              label={showLabel ? time : ''}
-              value={timer[time]}
-            />
-            {showColon && time !== 'seconds' && <span>:</span>}
-          </>
+          <React.Fragment key={time}>
+            <TimerBlock label={showLabel ? time : ''} value={timer[time]} />
+            {time !== 'seconds' && (
+              <span className={showColon ? '' : 'hidden'}>:</span>
+            )}
+          </React.Fragment>
         )
       })}
     </div>
