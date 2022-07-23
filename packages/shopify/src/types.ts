ad57d9b08dedb15d0ca04ef56caa8a7e19a3c6d0
Leo Huynh <huynhtuananh21895@gmail.com>
2022-07-24 00:31:53 +0700

    feat(shopify): add shopify package types

---
+++ packages/shopify/src/types.ts
@@ -0,0 +1,41 @@
+import { WeaverseElementProps } from '@weaverse/react'
+
+export type Product = {
+  id: number
+  title: string
+  body_html: string
+  vendor: string
+  handle: string
+}
+
+export type ProductContextProps = {
+  product?: Product
+  productId?: string | number
+}
+
+export interface ProductBoxProps extends WeaverseElementProps {
+  productId: number
+  productHandle?: string
+}
+
+export interface FormElementProps extends WeaverseElementProps {
+  formType: string
+  fields: FormField[]
+  button: FormSubmitButton
+}
+
+export type FormField = {
+  id: number
+  type: 'text' | 'email' | 'multiline'
+  placeholder: string
+  showLabel: boolean
+  label: string
+  required: boolean
+}
+
+export type FormSubmitButton = {
+  text: string
+  position: 'flex-start' | 'flex-end' | 'center'
+  openInNewTab: boolean
+  targetLink: string
+}
