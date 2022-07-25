03e681de4ffc1d8784c3a2c75bd73cba3c1c4506
Leo Huynh <huynhtuananh21895@gmail.com>
2022-07-25 15:55:28 +0700

    feat: add TimerBlock, Placeholder props types

--- packages/react/src/elements/shared/Placeholder.tsx
+++ packages/react/src/elements/shared/Placeholder.tsx
@@ -1,22 +1,25 @@
-import { TODO } from '@weaverse/core'
 import React, { forwardRef } from 'react'
+import type { PlaceholderProps } from '../../types'
 
-const Placeholder = forwardRef<HTMLElement, TODO>((props, ref) => {
-  let { element, children, ...rest } = props
-  let style = {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#374151',
+const Placeholder = forwardRef<HTMLDivElement, PlaceholderProps>(
+  (props, ref) => {
+    let { element, children, ...rest } = props
+    let style = {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: '#374151',
+    } as React.CSSProperties
+
+    return (
+      <div style={style} ref={ref} {...rest}>
+        <div style={{ fontWeight: 600, marginBottom: '10px' }}>{element}</div>
+        <div>{children}</div>
+      </div>
+    )
   }
-  return (
-    <div style={style} ref={ref} {...rest}>
-      <div style={{ fontWeight: 600, marginBottom: '10px' }}>{element}</div>
-      <div>{children}</div>
-    </div>
-  )
-})
+)
 
 export default Placeholder
