06897c23cc20bc6da67f6a95955d998310182457
ken <ken@weaverse.io>
2022-08-22 15:44:18 +0700

    chore: move default styles placeholder into builder

--- packages/react/src/elements/shared/Placeholder.tsx
+++ packages/react/src/elements/shared/Placeholder.tsx
@@ -3,26 +3,14 @@ import type { PlaceholderProps } from '~/types'
 
 const Placeholder = forwardRef<HTMLDivElement, PlaceholderProps>(
   (props, ref) => {
-    let { element, children, style: newStyle, ...rest } = props
-    let defaultStyle = {
-      position: 'absolute',
-      height: '100%',
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      pointerEvents: 'none',
-      color: '#374151',
-      backgroundColor: '#F9FAFB',
-      backgroundClip: 'content-box',
-      padding: 10,
-      border: '1px dashed #0F71FF',
-      borderRadius: 4,
-    } as React.CSSProperties
-    const style = { ...defaultStyle, ...newStyle }
+    let { element, children, ...rest } = props
     return (
-      <div data-wv-type="placeholder" style={style} ref={ref} {...rest}>
+      <div
+        data-wv-type="placeholder"
+        style={{ display: 'none' }}
+        ref={ref}
+        {...rest}
+      >
         <div style={{ fontWeight: 600, marginBottom: '10px' }}>{element}</div>
         <div>{children}</div>
       </div>
