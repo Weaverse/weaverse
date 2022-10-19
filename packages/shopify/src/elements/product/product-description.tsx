fef7386b09cf5b79d3b030ddd8b0cffeb48337e3
hta218 <huynhtuananh21895@gmail.com>
2022-10-19 19:52:42 +0700

    feat: add `ProductDescription`

---
+++ packages/shopify/src/elements/product/product-description.tsx
@@ -0,0 +1,50 @@
+import type { ElementCSS } from '@weaverse/react'
+import React, { forwardRef, useContext } from 'react'
+import { ProductContext } from '~/context'
+import type { ProductDescriptionProps } from '~/types'
+
+let ProductDescription = forwardRef<HTMLDivElement, ProductDescriptionProps>(
+  (props, ref) => {
+    let { lineClamp, children, ...rest } = props
+    let context = useContext(ProductContext)
+
+    if (context) {
+      let { product, ssrMode } = context
+      let style = {
+        WebkitBoxOrient: 'vertical',
+        WebkitLineClamp: lineClamp,
+      } as React.CSSProperties
+
+      if (ssrMode) {
+        return (
+          <div ref={ref} {...rest} style={style}>
+            {`{{ wv_product.description }}`}
+          </div>
+        )
+      }
+
+      return (
+        <div
+          ref={ref}
+          {...rest}
+          style={style}
+          dangerouslySetInnerHTML={{ __html: product.body_html }}
+        />
+      )
+    }
+    return null
+  }
+)
+
+export let css: ElementCSS = {
+  '@desktop': {
+    overflow: 'hidden',
+    display: '-webkit-box',
+  },
+}
+
+ProductDescription.defaultProps = {
+  lineClamp: 3,
+}
+
+export default ProductDescription
