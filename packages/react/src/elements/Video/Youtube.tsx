92d4f05e92182e728f5c7eb30900600423ffcd22
hta218 <huynhtuananh21895@gmail.com>
2022-11-15 15:24:23 +0700

    refactor: update react elements

--- packages/react/src/elements/Video/Youtube.tsx
+++ packages/react/src/elements/Video/Youtube.tsx
@@ -3,10 +3,13 @@ import type { YoutubeElementProps } from '~/types'
 
 export function Youtube(props: YoutubeElementProps) {
   let { youtubeId, controls, autoPlay, loop, muted } = props
-  let params = `autoplay=${autoPlay ? 1 : 0}&controls=${
-    controls ? 1 : 0
-  }&loop=${loop ? 1 : 0}&mute=${muted ? 1 : 0}`
-  let youtubeSrc = `https://www.youtube.com/embed/${youtubeId}?${params}`
+  let params = new URLSearchParams({
+    autoplay: autoPlay ? '1' : '0',
+    controls: controls ? '1' : '0',
+    loop: loop ? '1' : '0',
+    mute: muted ? '1' : '0',
+  })
+  let youtubeSrc = `https://www.youtube.com/embed/${youtubeId}?${params.toString()}`
   let allow = `accelerometer;${
     autoPlay ? 'autoplay;' : ''
   } clipboard-write; encrypted-media; gyroscope; picture-in-picture`
