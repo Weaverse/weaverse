5bac4c8592ef25bc488f06b246118beecb0a2520
hta218 <huynhtuananh21895@gmail.com>
2022-11-15 09:59:10 +0700

    feat: add `useProduct` hook

---
+++ packages/shopify/src/elements/product/ProductDetails/useProduct.ts
@@ -0,0 +1,44 @@
+import { WeaverseContext } from '@weaverse/react'
+import { useContext, useEffect, useRef, useState } from 'react'
+import { weaverseShopifyProducts } from '~/proxy'
+import type { ShopifyProduct, ShopifyProductVariant } from '~/types'
+import { updateProductData } from '~/utils'
+
+export function useProduct(
+  productId: number | 'default',
+  useDefaultProduct: boolean
+) {
+  let { isDesignMode } = useContext(WeaverseContext)
+  let formRef = useRef<HTMLFormElement>(null)
+
+  let product: ShopifyProduct = weaverseShopifyProducts[productId]
+  if (useDefaultProduct && !isDesignMode) {
+    let defaultProduct = weaverseShopifyProducts['default']
+    if (defaultProduct?.id) {
+      product = defaultProduct
+    }
+  }
+
+  let [selectedVariant, setSelectedVariant] =
+    useState<ShopifyProductVariant | null>(null)
+
+  useEffect(() => {
+    if (product) {
+      updateProductData(product)
+      setSelectedVariant(
+        product.selected_or_first_available_variant || product.variants[0]
+      )
+      window.weaverseCartHelpers?.notify('on_product_rendered', {
+        product,
+        formRef,
+      })
+    }
+  }, [product])
+
+  return {
+    product,
+    formRef,
+    selectedVariant,
+    setSelectedVariant,
+  }
+}
