b98a5e1d82c28faab7670182c461619accafc308
hta218 <huynhtuananh21895@gmail.com>
2022-12-13 08:00:51 +0700

    feat(core): update utils

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,6 +1,13 @@
-export * from "./load-script"
 export * from "./styles"
 
 export const isReactNative = typeof navigator === "object" && navigator.product === "ReactNative"
 export const isBrowser = typeof window !== "undefined" && !isReactNative
 export const isIframe = isBrowser && window.top !== window.self
+
+export function merge(target: Record<string, any>, source: Record<string, any>) {
+  for (let key of Object.keys(source)) {
+    if (source[key] instanceof Object) Object.assign(source[key], merge(target[key], source[key]))
+  }
+  Object.assign(target || {}, source)
+  return target
+}
