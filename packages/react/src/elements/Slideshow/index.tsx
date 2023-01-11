5f64e62c98f1a7b71254b0299a275a5b57e5e0c7
hta218 <huynhtuananh21895@gmail.com>
2023-01-11 15:56:38 +0700

    feat(slideshow): return settings from `useSlideshowConfigs` hook

--- packages/react/src/elements/Slideshow/index.tsx
+++ packages/react/src/elements/Slideshow/index.tsx
@@ -1,73 +1,24 @@
 import { css as stitchesCss } from '@stitches/react'
 import type { ElementCSS } from '@weaverse/core'
 import clsx from 'clsx'
-import { useKeenSlider } from 'keen-slider/react'
-import React, { forwardRef, useContext, useEffect, useState } from 'react'
+import React, { forwardRef } from 'react'
 import { Arrows } from '~/components/Slider/Arrows'
 import { Dots } from '~/components/Slider/Dots'
-import { ResizePlugin } from '~/components/Slider/ResizePlugin'
-import { WeaverseContext } from '~/context'
 import type { SlideshowProps } from '~/types'
-import { loadCSS } from '~/utils/css'
+import { useSlideshowConfigs } from './useSlideshowConfigs'
 
 let Slideshow = forwardRef<HTMLDivElement, SlideshowProps>((props, ref) => {
   let {
     animation,
-    slidesPerView,
-    spacing,
     showArrows,
     showDots,
     dotsPosition,
     dotsColor,
-    loop,
-    autoRotate,
-    changeSlidesEvery,
     children,
     ...rest
   } = props
-  let { isDesignMode } = useContext(WeaverseContext)
-  let [opacities, setOpacities] = React.useState<number[]>([])
-  let [cssLoaded, setCssLoaded] = useState(false)
-  let [created, setCreated] = useState(false)
-  let [ready, setReady] = useState(false)
-  let [currentSlide, setCurrentSlide] = useState(0)
-  let [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(
-    {
-      slides:
-        animation === 'fade'
-          ? React.Children.count(children)
-          : { perView: slidesPerView },
-      drag: !isDesignMode,
-      loop,
-      selector: animation === 'fade' ? null : '.keen-slider__slide',
-      created: () => {
-        setCreated(true)
-      },
-      slideChanged: (slider) => {
-        setCurrentSlide(slider.track.details.rel)
-      },
-      detailsChanged: (slider) => {
-        let newOpacities = slider.track?.details?.slides?.map(
-          (slide) => slide.portion
-        )
-        setOpacities(newOpacities)
-      },
-    },
-    [ResizePlugin]
-  )
-
-  useEffect(() => {
-    loadCSS({
-      rel: 'stylesheet',
-      href: 'https://cdn.jsdelivr.net/npm/keen-slider@latest/keen-slider.min.css',
-    }).then(() => setCssLoaded(true))
-  }, [])
-
-  useEffect(() => {
-    if (created && cssLoaded) {
-      setReady(true)
-    }
-  }, [created, cssLoaded])
+  let { sliderRef, instanceRef, currentSlide, opacities, ready, created } =
+    useSlideshowConfigs(props)
 
   let style = {
     '--slider-opacity': ready ? 1 : 0,
