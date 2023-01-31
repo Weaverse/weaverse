1942218a9a7dabeffb132ff1c3e6155537a701d6
hta218 <huynhtuananh21895@gmail.com>
2023-01-31 18:34:02 +0700

    feat(react): add icons

--- packages/react/src/components/icons/phosphor.tsx
+++ packages/react/src/components/icons/phosphor.tsx
@@ -195,7 +195,7 @@ export let ShoppingCart = (props: HTMLAttributes<SVGElement>) => {
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="16"
-      ></path>
+      />
       <circle
         cx="80"
         cy="204"
@@ -205,7 +205,7 @@ export let ShoppingCart = (props: HTMLAttributes<SVGElement>) => {
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="16"
-      ></circle>
+      />
       <circle
         cx="184"
         cy="204"
@@ -215,7 +215,7 @@ export let ShoppingCart = (props: HTMLAttributes<SVGElement>) => {
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="16"
-      ></circle>
+      />
       <path
         d="M62.5,144H188.1a15.9,15.9,0,0,0,15.7-13.1L216,64H48"
         fill="none"
@@ -223,7 +223,62 @@ export let ShoppingCart = (props: HTMLAttributes<SVGElement>) => {
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="16"
-      ></path>
+      />
+    </svg>
+  )
+}
+
+export let Storefront = (props: HTMLAttributes<SVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="192"
+      height="192"
+      fill="currentColor"
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <rect width="256" height="256" fill="none"></rect>
+      <path
+        d="M48,139.6V208a8,8,0,0,0,8,8H200a8,8,0,0,0,8-8V139.6"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="8"
+      />
+      <path
+        d="M54,40H202a8.1,8.1,0,0,1,7.7,5.8L224,96H32L46.3,45.8A8.1,8.1,0,0,1,54,40Z"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="8"
+      />
+      <path
+        d="M96,96v16a32,32,0,0,1-64,0V96"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="8"
+      />
+      <path
+        d="M160,96v16a32,32,0,0,1-64,0V96"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="8"
+      />
+      <path
+        d="M224,96v16a32,32,0,0,1-64,0V96"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="8"
+      />
     </svg>
   )
 }
