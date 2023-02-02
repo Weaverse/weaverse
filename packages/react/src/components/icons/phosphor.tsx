af30a5be1bc33ffef39acb1e6a54dc418c4fa52d
hta218 <huynhtuananh21895@gmail.com>
2023-02-02 22:47:21 +0700

    chore: update icons

--- packages/react/src/components/icons/phosphor.tsx
+++ packages/react/src/components/icons/phosphor.tsx
@@ -405,6 +405,59 @@ export let Backpack = (props: HTMLAttributes<SVGElement>) => {
   )
 }
 
+export let Newspaper = (props: HTMLAttributes<SVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="192"
+      height="192"
+      fill="currentColor"
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <rect width="256" height="256" fill="none" />
+      <line
+        x1="96"
+        y1="112"
+        x2="176"
+        y2="112"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="8"
+      />
+      <line
+        x1="96"
+        y1="144"
+        x2="176"
+        y2="144"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="8"
+      />
+      <path
+        d="M32,200a16,16,0,0,0,16-16V64a8,8,0,0,1,8-8H216a8,8,0,0,1,8,8V184a16,16,0,0,1-16,16Z"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="8"
+      />
+      <path
+        d="M32,200a16,16,0,0,1-16-16h0V88"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="8"
+      />
+    </svg>
+  )
+}
+
 export let Eye = (props: HTMLAttributes<SVGElement>) => {
   return (
     <svg
