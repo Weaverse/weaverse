a2cf6ad9f70106f6e5b36ccb2ae20dcc30ae7413
Paul <phanminh65@gmail.com>
2023-04-12 16:11:44 +0700

    feat: updating mechanism

--- packages/hydrogen/src/weaverse-loader.ts
+++ packages/hydrogen/src/weaverse-loader.ts
@@ -5,7 +5,7 @@ export async function weaverseLoader({
   params,
 }: LoaderArgs): Promise<any> {
   /**
-   * @todo read the url and params from the request
+   * @todo read the url and params from the request => JSON DATA (items, installed add-ons)
    * @todo load the weaverse project page data by current url and projectId
    * @todo read the page data to detech whether items data need 3rd party api call (shopify data, etc)
    * @todo with data from Shopify, we can get it by using context.storefront.query(graphqlQuery)
--- packages/react/src/renderer.tsx
+++ packages/react/src/renderer.tsx
@@ -28,7 +28,6 @@ export let WeaverseRoot = ({ context }: WeaverseRootPropsType) => {
       <div
         className={`weaverse-content-root ${themeClass}`}
         {...eventHandlers}
-        suppressHydrationWarning
         ref={rootRef}
       >
         <WeaverseContextProvider value={context}>
