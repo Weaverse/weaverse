4ed80c058045077d2ab634f8f76a636f83efc1b3
Paul <phanminh65@gmail.com>
2023-04-15 12:13:55 +0700

    feat: updating preview, sdks

--- packages/hydrogen/src/weaverse-loader.ts
+++ packages/hydrogen/src/weaverse-loader.ts
@@ -10,7 +10,7 @@ export async function weaverseLoader({
    * @todo read the page data to detech whether items data need 3rd party api call (shopify data, etc)
    * @todo with data from Shopify, we can get it by using context.storefront.query(graphqlQuery)
    * @todo if items data need 3rd party api call, call the api and return the data
-   * @todo the returned data format will be {weaverseData: {}, 3rdPartyData: {}, products: [], collections: [], product: {}, collection: {}, etc}
+   * @todo the returned data format will be {weaversePageData: {}, 3rdPartyData: {}, products: [], collections: [], product: {}, collection: {}, etc}
    */
   return {
     foo: 'bar',
