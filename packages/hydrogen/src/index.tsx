a7560378dbc889d89c52a52f85d172e678f29bb1
Paul <phanminh65@gmail.com>
2023-04-25 10:16:36 +0700

    feat: ensuring stitches work on production

--- packages/hydrogen/src/index.tsx
+++ packages/hydrogen/src/index.tsx
@@ -1,5 +1,5 @@
 import { useStudio } from './utils'
-import React, { memo, useEffect, useState } from 'react'
+import React, { memo, useEffect } from 'react'
 import type { WeaverseComponentsType } from './types'
 import { WeaverseRoot } from '@weaverse/react'
 import type { Weaverse } from '@weaverse/core'
@@ -19,12 +19,28 @@ export let WeaverseHydrogenRoot = memo(
   }) => {
     let weaverse = createWeaverseHydrogenContext(data, components)
     useStudio(weaverse)
+    useEnsureStitchesWorking(weaverse)
     if (!weaverse?.data) {
       return <div>404</div>
     }
     return <WeaverseRoot context={weaverse} />
   }
 )
+
+let useEnsureStitchesWorking = (weaverse: Weaverse) => {
+  useEffect(() => {
+    let stitchesInstance = weaverse.stitchesInstance
+    if (stitchesInstance && !stitchesInstance.sheet?.sheet?.ownerNode) {
+      console.warn('stitches instance is not working, re-creating it')
+      //this means that the stitches instance is not working
+      // we will re-create it
+      delete weaverse.stitchesInstance
+      weaverse.initStitches({ root: document })
+      weaverse.triggerUpdate()
+      weaverse.refreshAllItems()
+    }
+  }, [])
+}
 
 // export function useIsHydrated() {
 //   const [isHydrated, setHydrated] = useState<boolean>(false)
@@ -35,10 +51,18 @@ export let WeaverseHydrogenRoot = memo(
 
 //   return isHydrated
 // }
-// let RenderStitchesStyles = ({ weaverse }: { weaverse: Weaverse }) => {
+// let NoHydrate = Components.NoHydrate
+// let RenderStitchesStyles = memo(({ weaverse }: { weaverse: Weaverse }) => {
 //   // let h = useIsHydrated()
-//   // useEffect(() => {
-//   //   h && !weaverse.isDesignMode && weaverse.triggerUpdate()
-//   // }, [h])
-//   return null
-// }
+//   useEffect(() => {
+//     weaverse.stitchesInstance.reset()
+//   }, [])
+//   return isBrowser ? null : (
+//     <NoHydrate
+//       id="weaverse-styles"
+//       getHTML={() =>
+//         `<style id="stitches">${weaverse.stitchesInstance.getCssText()}</style>`
+//       }
+//     />
+//   )
+// })
