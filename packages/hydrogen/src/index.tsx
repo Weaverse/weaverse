cb2157ced238070133e6cd5d9e6e9e5ed1e1ec6c
Paul <phanminh65@gmail.com>
2023-04-25 10:29:13 +0700

    feat: making stitches work on production

--- packages/hydrogen/src/index.tsx
+++ packages/hydrogen/src/index.tsx
@@ -35,34 +35,9 @@ let useEnsureStitchesWorking = (weaverse: Weaverse) => {
       //this means that the stitches instance is not working
       // we will re-create it
       delete weaverse.stitchesInstance
-      weaverse.initStitches({ root: document })
+      weaverse.initStitches({ nounce: Date.now() })
       weaverse.triggerUpdate()
       weaverse.refreshAllItems()
     }
   }, [])
 }
-
-// export function useIsHydrated() {
-//   const [isHydrated, setHydrated] = useState<boolean>(false)
-
-//   useEffect(() => {
-//     setHydrated(true)
-//   }, [])
-
-//   return isHydrated
-// }
-// let NoHydrate = Components.NoHydrate
-// let RenderStitchesStyles = memo(({ weaverse }: { weaverse: Weaverse }) => {
-//   // let h = useIsHydrated()
-//   useEffect(() => {
-//     weaverse.stitchesInstance.reset()
-//   }, [])
-//   return isBrowser ? null : (
-//     <NoHydrate
-//       id="weaverse-styles"
-//       getHTML={() =>
-//         `<style id="stitches">${weaverse.stitchesInstance.getCssText()}</style>`
-//       }
-//     />
-//   )
-// })
