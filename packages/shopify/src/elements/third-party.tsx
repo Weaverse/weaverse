3dafdd6a68b3666c09a470414e73edf225b4b147
Paul <phanminh65@gmail.com>
2023-05-18 12:47:48 +0700

    chore: fix build

--- packages/shopify/src/elements/third-party.tsx
+++ packages/shopify/src/elements/third-party.tsx
@@ -1,9 +1,10 @@
 import React, { forwardRef, useContext } from 'react'
 import type { ThirdPartyProps } from '~/types'
-import { NoHydrate } from '@weaverse/react/src/components/no-hydrate'
+import { Components } from '@weaverse/react'
 import { WeaverseContext } from '@weaverse/react'
-import Placeholder from '@weaverse/react/src/components/placeholder'
 
+let NoHydrate = Components.NoHydrate
+let Placeholder = Components.Placeholder
 let ThirdParty = forwardRef<HTMLDivElement, ThirdPartyProps>((props, ref) => {
   const { snippet_code, information, placeholder, ...rest } = props
   const id = rest['data-wv-id'] as string
