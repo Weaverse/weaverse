e2a0e50b574939dfc2e1df73f13cded14b53960d
hta218 <huynhtuananh21895@gmail.com>
2023-08-04 16:20:28 +0700

    chore(hydrogen): simplify use studio

--- packages/hydrogen/src/hooks/use-studio.ts
+++ packages/hydrogen/src/hooks/use-studio.ts
@@ -1,46 +1,23 @@
-import { isIframe, loadScript } from '@weaverse/react'
+import { useNavigate, useRevalidator } from '@remix-run/react'
+import { isIframe } from '@weaverse/react'
 import { useEffect } from 'react'
-import { useRevalidator, useNavigate } from '@remix-run/react'
-import type { HydrogenThemeSchema, WeaverseHydrogen } from '~/types'
+import type { WeaverseHydrogen } from '~/types'
 
-export function useStudio(
-  weaverse: WeaverseHydrogen,
-  themeSchema: HydrogenThemeSchema,
-) {
-  let revalidator = useRevalidator()
+export function useStudio(weaverse: WeaverseHydrogen) {
   let navigate = useNavigate()
-  console.log('revalidator', revalidator)
+  let revalidator = useRevalidator()
 
   useEffect(() => {
-    let initialized = checkStudioInitialized(weaverse)
-    if (isIframe && weaverse.isDesignMode && !initialized) {
-      window.__initializedWeaverseStudios[weaverse.pageId] = true
-      weaverse.internal.themeSchema = themeSchema
-      weaverse.internal.navigate = navigate
-      weaverse.internal.revalidator = revalidator
-
-      let host = weaverse.weaverseHost
-      let version = weaverse.weaverseVersion || Date.now()
-      loadScript(`${host}/assets/studio/studio-bridge.js?v=${version}`).then(
-        () => {
-          window?.createWeaverseStudioBridge(weaverse)
-          setTimeout(() => {
-            weaverse.triggerUpdate()
-          }, 2000)
-        },
-      )
+    if (isIframe && weaverse.isDesignMode) {
+      weaverse.internal = {
+        ...weaverse.internal,
+        navigate,
+        revalidator,
+      }
+      window.__weaverse = weaverse
+      if (window.weaverseStudio) {
+        window.weaverseStudio.init(weaverse)
+      }
     }
-    window.__weaverse = weaverse
   }, [weaverse])
-}
-
-function checkStudioInitialized(weaverseInit: WeaverseHydrogen) {
-  window.__initializedWeaverseStudios =
-    window.__initializedWeaverseStudios || {}
-  let { pageId } = weaverseInit
-  let initialized = window.__initializedWeaverseStudios?.[pageId]
-  if (initialized) {
-    return true
-  }
-  return false
 }
