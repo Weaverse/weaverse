d86aa39b77651732f5f48ec10371e5f8e24d31e9
hta218 <huynhtuananh21895@gmail.com>
2023-08-04 21:23:53 +0700

    chore(core): add request info

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -145,6 +145,7 @@ export class Weaverse {
   pageId = ""
 
   internal: any = {}
+  requestInfo: any = {}
   /**
    * Weaverse project data, by default, user can provide project data via React Component:
    * <WeaverseRoot data={data} /> it will be used to server-side rendering
