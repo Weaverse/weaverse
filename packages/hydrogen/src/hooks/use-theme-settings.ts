46674390905c2d8bd729fd017774e1617db08ded
hta218 <huynhtuananh21895@gmail.com>
2023-08-30 15:54:01 +0700

    feat: return theme settings only from `useThemeSettings`

--- packages/hydrogen/src/hooks/use-theme-settings.ts
+++ packages/hydrogen/src/hooks/use-theme-settings.ts
@@ -1,16 +1,10 @@
 import { useWeaverse } from '@weaverse/react'
-import type { WeaverseHydrogen } from '~/types'
+import type { HydrogenProjectConfig, WeaverseHydrogen } from '~/types'
 
-export function useThemeSettings() {
+type WeaverseThemeSettings = HydrogenProjectConfig['theme']
+
+export function useThemeSettings<T = WeaverseThemeSettings>() {
   let weaverse = useWeaverse<WeaverseHydrogen>()
   let settings = weaverse?.internal?.project?.config?.theme
-  let updateSettings = (newSettings: any) => {
-    weaverse.internal.project.config.theme = Object.assign(
-      {},
-      settings,
-      newSettings,
-    )
-    weaverse.triggerUpdate()
-  }
-  return [settings, updateSettings]
+  return settings as T
 }
