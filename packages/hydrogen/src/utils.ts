a95bfb3560df964175d59f72a6c28ddb5b81bb68
hta218 <huynhtuananh21895@gmail.com>
2023-09-12 09:10:08 +0700

    feat(hydrogen): get weaverse csp configs

--- packages/hydrogen/src/utils.ts
+++ packages/hydrogen/src/utils.ts
@@ -10,3 +10,29 @@ export function getRequestQueries<T = Record<string, string | boolean>>(
     {},
   ) as T
 }
+
+export function getWeaverseCsp(request: Request) {
+  let url = new URL(request.url)
+  // get weaverse host from query params
+  let weaverseHost = url.searchParams.get('weaverseHost')
+  let weaverseHosts = ['https://*.weaverse.io']
+  if (weaverseHost) {
+    weaverseHosts.push(weaverseHost)
+  }
+  return {
+    frameAncestors: weaverseHosts,
+    defaultSrc: [
+      "'self'",
+      'https://cdn.shopify.com',
+      'https://shopify.com',
+      'https://*.youtube.com',
+      ...weaverseHosts,
+    ],
+    styleSrc: [
+      "'self'",
+      "'unsafe-inline'",
+      'https://cdn.shopify.com',
+      ...weaverseHosts,
+    ],
+  }
+}
