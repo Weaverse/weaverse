6b7cd74d5fcbaa6562aa53d79b6dacf43fc87d9e
hta218 <huynhtuananh21895@gmail.com>
2023-09-16 17:31:51 +0700

    chore: clean up

---
+++ packages/hydrogen/src/index.ts
@@ -0,0 +1,20 @@
+// import { Weaverse } from '@weaverse/react'
+// import type { WeaverseInternal } from './types'
+
+export * from '@weaverse/react'
+export * from './client'
+export * from './hooks/use-theme-settings'
+export * from './types'
+export * from './utils'
+export * from './wrappers'
+export * from './WeaverseHydrogenRoot'
+
+// TODO
+// export class WeaverseHydrogen extends Weaverse {
+//   internal: WeaverseInternal | null = null
+
+//   constructor({ internal }: { internal: WeaverseInternal }) {
+//     super()
+//     this.internal = internal
+//   }
+// }
