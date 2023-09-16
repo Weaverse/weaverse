fe420e4218e4c70578352ff52f564e574f7a88d3
hta218 <huynhtuananh21895@gmail.com>
2023-09-16 07:54:52 +0700

    feat(hydrogen): get initial weaverse client configs

--- packages/hydrogen/src/utils.ts
+++ packages/hydrogen/src/utils.ts
@@ -1,3 +1,9 @@
+import type {
+  HydrogenThemeEnv,
+  WeaverseProjectConfigs,
+  WeaverseStudioQueries,
+} from './types'
+
 export function getRequestQueries<T = Record<string, string | boolean>>(
   request: Request,
 ) {
@@ -9,6 +15,32 @@ export function getRequestQueries<T = Record<string, string | boolean>>(
     },
     {},
   ) as T
+}
+
+export function getWeaverseConfigs(
+  request: Request,
+  env: HydrogenThemeEnv,
+): WeaverseProjectConfigs {
+  let queries = getRequestQueries<WeaverseStudioQueries>(request)
+  let {
+    WEAVERSE_PROJECT_ID,
+    WEAVERSE_HOST,
+    PUBLIC_STORE_DOMAIN,
+    PUBLIC_STOREFRONT_API_TOKEN,
+  } = env || {}
+  let { weaverseProjectId, weaverseHost, isDesignMode, weaverseVersion } =
+    queries
+
+  return {
+    projectId: weaverseProjectId || WEAVERSE_PROJECT_ID || '',
+    weaverseHost: weaverseHost || WEAVERSE_HOST || 'https://weaverse.io',
+    weaverseVersion: weaverseVersion || '',
+    isDesignMode: isDesignMode || false,
+    publicEnv: {
+      PUBLIC_STORE_DOMAIN,
+      PUBLIC_STOREFRONT_API_TOKEN,
+    },
+  }
 }
 
 export function getWeaverseCsp(request: Request) {
