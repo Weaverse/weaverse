e7b309ced15df3360d83761c3acb915179f8b54f
hta218 <huynhtuananh21895@gmail.com>
2023-09-30 10:01:05 +0700

    feat(core): add `EventEmitter`

---
+++ packages/core/src/utils/event-emiiter.ts
@@ -0,0 +1,19 @@
+type AnyFunction = (...args: any[]) => any
+
+export class EventEmitter {
+  listeners: Set<AnyFunction>
+  constructor() {
+    this.listeners = new Set()
+  }
+  subscribe(fn: AnyFunction) {
+    this.listeners.add(fn)
+  }
+  unsubscribe(fn: AnyFunction) {
+    this.listeners.delete(fn)
+  }
+  emit(data?: any) {
+    this.listeners.forEach((fn) => {
+      return fn(data)
+    })
+  }
+}
