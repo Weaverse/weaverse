d12b9956f4e293dc9e0339626eaf7c2f4c35ede4
hta218 <huynhtuananh21895@gmail.com>
2023-10-01 22:52:42 +0700

    refactor(core): move shopify types to shopify packages

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -28,87 +28,75 @@ export interface WeaverseCoreParams {
 export interface WeaverseElement {
   Component: ForwardRefExoticComponent<any>
   type: string
-  schema?: ElementSchema
-  defaultCss?: ElementCSS
-  permanentCss?: ElementCSS
-  extraData?: ElementExtraData
+  // schema?: ElementSchema
+  // defaultCss?: ElementCSS
+  // permanentCss?: ElementCSS
+  // extraData?: ElementExtraData
 }
 
-// Element types
-export type CatalogGroup = "essential" | "composition" | "shopify"
-export interface ElementCatalog {
-  name: string
-  icon?: string
-  group?: CatalogGroup
-  data?: ElementDataInCatalog[]
-}
-export interface ElementDataInCatalog extends Omit<ElementData, "id"> {
-  id: string | number
-}
+// export type FlagType =
+//   | "draggable"
+//   | "resizable"
+//   | "sortable"
+//   | "ignoreShortcutKeys"
+//   | "hasContextMenu"
+//   | "isSortableContext"
+//   | "mustHaveChildren"
+// export type ElementFlags = Partial<Record<FlagType, boolean>>
 
-export type FlagType =
-  | "draggable"
-  | "resizable"
-  | "sortable"
-  | "ignoreShortcutKeys"
-  | "hasContextMenu"
-  | "isSortableContext"
-  | "mustHaveChildren"
-export type ElementFlags = Partial<Record<FlagType, boolean>>
+// export type ChildElementSelector = string | string[]
 
-export type ChildElementSelector = string | string[]
+// export interface ChildElement {
+//   label: string
+//   selector: ChildElementSelector
+// }
+// export type ParentType = "container" | "layout" | "root" | "product-details" | "product-info" | "slideshow" | "slide"
 
-export interface ChildElement {
-  label: string
-  selector: ChildElementSelector
-}
-export type ParentType = "container" | "layout" | "root" | "product-details" | "product-info" | "slideshow" | "slide"
-
-export type GridSize = {
-  rowSpan: number
-  colSpan: number
-}
+// export type GridSize = {
+//   rowSpan: number
+//   colSpan: number
+// }
 export type ToolbarAction =
   | "general-settings"
   | "settings-level-2"
-  | "text-presets"
-  | "ai-assistant"
-  | "scale-text"
+  // | "text-presets"
+  // | "ai-assistant"
+  // | "scale-text"
   | "duplicate"
   | "delete"
-  | "copy-styles"
-  | "paste-styles"
-  | "move-up"
-  | "move-down"
-  | "next-slide"
-  | "prev-slide"
-  | "change-background"
-  | "toggle-visibility"
-  | "more-actions"
+// | "copy-styles"
+// | "paste-styles"
+// | "move-up"
+// | "move-down"
+// | "next-slide"
+// | "prev-slide"
+// | "change-background"
+// | "toggle-visibility"
+// | "more-actions"
 
 export interface ElementSchema {
   title: string
   type: string
-  parentTypes: ParentType[]
-  gridSize?: GridSize
-  inspector?: ElementInspector
-  toolbar?: (ToolbarAction | ToolbarAction[])[]
-  childElements?: ChildElement[]
-  catalog?: ElementCatalog
-  flags?: ElementFlags
+  // parentTypes: ParentType[]
+  // gridSize?: GridSize
+  // inspector?: ElementInspector
+  // toolbar?: (ToolbarAction | ToolbarAction[])[]
+  // childElements?: ChildElement[]
+  // catalog?: ElementCatalog
+  // flags?: ElementFlags
 }
 
 export interface ElementData {
   id: string
   type: string
-  childIds?: (string | number)[]
-  css?: ElementCSS
+  // childIds?: (string | number)[]
+  // css?: ElementCSS
   [key: string]: any
 }
 
-export interface ElementExtraData {
-  [key: string]: unknown
-}
+// export interface ElementExtraData {
+//   [key: string]: unknown
+// }
 
 export type WeaverseCSSProperties = Stitches.CSS & Partial<Record<keyof typeof stitchesUtils, string | number>>
 export type ChildElementCSS = Partial<{
@@ -119,21 +107,20 @@ export interface ElementCSS {
   "@mobile"?: WeaverseCSSProperties | ChildElementCSS
 }
 
-export interface ElementInspector {
-  settings?: (AdvancedGroup | BasicGroup)[]
-  styles?: (AdvancedGroup | BasicGroup)[]
-}
+// export interface ElementInspector {
+//   settings?: (AdvancedGroup | BasicGroup)[]
+//   styles?: (AdvancedGroup | BasicGroup)[]
+// }
 
-export interface AdvancedGroup {
-  groupType: AdvancedGroupType
-  useData?: boolean
-}
+// export interface AdvancedGroup {
+//   groupType: AdvancedGroupType
+// }
 
-export interface BasicGroup {
-  groupType: "basic"
-  groupHeader: string
-  inputs: BasicInput[]
-}
+// export interface BasicGroup {
+//   groupType: "basic"
+//   groupHeader: string
+//   inputs: BasicInput[]
+// }
 
 export type AdvancedGroupType =
   // Styles
@@ -150,7 +137,7 @@ export type AdvancedGroupType =
 export interface BasicInput<ConfigsType = AdditionalInputConfigs> {
   type: InputType
   label?: string
-  binding?: "style" | "data"
+  // binding?: "style" | "data"
   /**
    * The key of the value of the element data or styles
    * @example
@@ -180,7 +167,7 @@ export interface BasicInput<ConfigsType = AdditionalInputConfigs> {
   defaultValue?: string | number | boolean
   placeholder?: string
   helpText?: string
-  shouldRevalidate?: boolean
+  // shouldRevalidate?: boolean
 }
 
 export type AdditionalInputConfigs =
@@ -188,7 +175,7 @@ export type AdditionalInputConfigs =
   | ToggleGroupConfigs
   | RangeInputConfigs
   | ChildrenSortInputConfigs
-  | DataSortInputConfigs
+// | DataSortInputConfigs
 
 export interface SelectInputConfigs {
   options?: { value: string; label: string; icon?: string; weight?: string }[]
@@ -210,11 +197,11 @@ export interface ChildrenSortInputConfigs {
   actions: SortableItemAction[]
 }
 
-export interface DataSortInputConfigs {
-  prop: string
-  defaultData: object
-  inspector: string
-}
+// export interface DataSortInputConfigs {
+//   prop: string
+//   defaultData: object
+//   inspector: string
+// }
 
 export type InputType =
   // Basic inputs
@@ -224,24 +211,24 @@ export type InputType =
   | "range"
   | "select"
   | "children-sort"
-  | "data-sort"
   | "switch"
   | "text"
   | "textarea"
   | "toggle-group"
   | "position"
-  | "information"
-  // Element inputs
-  | "product"
-  | "product-list"
-  | "product-swatches"
-  | "text-editor"
-  | "custom.html"
-  | "instagram"
-  | "collection-list"
-  | "collection"
-  | "article-list"
-  | "map-autocomplete"
+// Element inputs
+// | "data-sort"
+// | "information"
+// | "product"
+// | "product-list"
+// | "product-swatches"
+// | "text-editor"
+// | "custom.html"
+// | "instagram"
+// | "collection-list"
+// | "collection"
+// | "article-list"
+// | "map-autocomplete"
 
 declare global {
   interface Window {
