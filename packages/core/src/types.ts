6ef6ab67916cfb4c08dca4d6172efe44b72bddf3
Paul <phanminh65@gmail.com>
2023-12-27 12:05:23 +0700

    Refactor types.ts

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -3,58 +3,9 @@ import type { ForwardRefExoticComponent } from 'react'
 import type { WeaverseItemStore } from './core'
 import type { stitchesUtils } from './utils/stitches'
 
-export interface WeaverseProjectDataType {
-  items: ElementData[]
-  rootId: string
-}
-
-export interface BreakPoints {
-  mobile: string
-  desktop: string
-}
-
+// Simple types
 export type PlatformTypeEnum = 'shopify-section' | 'shopify-hydrogen' | 'nextjs'
-
-export interface WeaverseCoreParams {
-  mediaBreakPoints?: BreakPoints
-  weaverseHost?: string
-  weaverseVersion?: string
-  projectId: string
-  data: WeaverseProjectDataType
-  isDesignMode?: boolean
-  platformType?: PlatformTypeEnum
-  ItemConstructor: typeof WeaverseItemStore
-}
-
-export interface WeaverseElement {
-  Component: ForwardRefExoticComponent<any>
-  type: string
-}
-
 export type ToolbarAction = 'general-settings' | 'settings-level-2' | 'duplicate' | 'delete'
-
-export interface ElementSchema {
-  title: string
-  type: string
-}
-
-export interface ElementData {
-  id: string
-  type: string
-
-  [key: string]: any
-}
-
-export type WeaverseCSSProperties = Stitches.CSS & Partial<Record<keyof typeof stitchesUtils, string | number>>
-export type ChildElementCSS = Partial<{
-  [selector: string]: WeaverseCSSProperties & ChildElementCSS
-}>
-
-export interface ElementCSS {
-  '@desktop'?: WeaverseCSSProperties | ChildElementCSS
-  '@mobile'?: WeaverseCSSProperties | ChildElementCSS
-}
-
 export type AdvancedGroupType =
   | 'border'
   | 'alignment'
@@ -65,71 +16,6 @@ export type AdvancedGroupType =
   | 'visibility'
   | 'shadows-and-effects'
   | 'layout-background'
-
-export interface BasicInput<ConfigsType = AdditionalInputConfigs> {
-  type: InputType
-  /**
-   * The key of the value of the element data or styles
-   * @example
-   * // Bind to `element.data.title`
-   * name: "title"
-   * // Bind to `element.css["@desktop"].backgroundColor`
-   * name: "backgroundColor"
-   */
-  name: string
-  label?: string
-  /**
-   * Additional options for inputs that require more configuration
-   */
-  configs?: ConfigsType
-  /**
-   * Only display if condition matches.
-   *
-   * Format: `bindingName.conditionalOperator.value`
-   *
-   * Supported operators: `eq`, `ne`, `gt`, `gte`, `lt`, `lte`
-   *
-   * @example
-   * `clickAction.eq.openLink`
-   * `clickAction.ne.openLink`
-   * `imagesPerRow.gt.1`
-   */
-  condition?: string
-  defaultValue?: string | number | boolean | Partial<WeaverseImage> | { [x: string]: any }
-  placeholder?: string
-  helpText?: string
-}
-
-export type WeaverseResourcePickerData = {
-  id: number
-  handle: string
-}
-
-export type WeaverseImage = {
-  id: string
-  url: string
-  altText: string
-  width: number
-  height: number
-}
-
-export type AdditionalInputConfigs = SelectInputConfigs | ToggleGroupConfigs | RangeInputConfigs
-
-export interface SelectInputConfigs {
-  options?: { value: string; label: string }[]
-}
-
-export interface ToggleGroupConfigs {
-  options?: { value: string; label: string; icon?: string; weight?: string }[]
-}
-
-export interface RangeInputConfigs {
-  min?: number
-  max?: number
-  step?: number
-  unit?: string
-}
-
 export type PositionInputValue =
   | 'top left'
   | 'top center'
@@ -140,7 +26,6 @@ export type PositionInputValue =
   | 'bottom left'
   | 'bottom center'
   | 'bottom right'
-
 export type InputType =
   | 'blog'
   | 'collection'
@@ -160,6 +45,95 @@ export type InputType =
   | 'textarea'
   | 'toggle-group'
   | 'swatches'
+
+// Complex types
+export type WeaverseCSSProperties = Stitches.CSS & Partial<Record<keyof typeof stitchesUtils, string | number>>
+export type ChildElementCSS = Partial<{ [selector: string]: WeaverseCSSProperties & ChildElementCSS }>
+export type WeaverseResourcePickerData = { id: number; handle: string }
+export type WeaverseImage = { id: string; url: string; altText: string; width: number; height: number }
+export type AdditionalInputConfigs = SelectInputConfigs | ToggleGroupConfigs | RangeInputConfigs
+
+export interface ElementCSS {
+  '@desktop'?: WeaverseCSSProperties | ChildElementCSS
+  '@mobile'?: WeaverseCSSProperties | ChildElementCSS
+}
+
+export interface ElementData {
+  id: string
+  type: string
+  [key: string]: any
+}
+
+export interface WeaverseElement {
+  Component: ForwardRefExoticComponent<any>
+  type: string
+}
+
+export interface ElementSchema {
+  title: string
+  type: string
+}
+
+export interface BreakPoints {
+  mobile: string
+  desktop: string
+}
+
+export interface WeaverseProjectDataType {
+  items: ElementData[]
+  rootId: string
+}
+
+export interface WeaverseCoreParams {
+  mediaBreakPoints?: BreakPoints
+  weaverseHost?: string
+  weaverseVersion?: string
+  projectId: string
+  data: WeaverseProjectDataType
+  isDesignMode?: boolean
+  platformType?: PlatformTypeEnum
+  ItemConstructor: typeof WeaverseItemStore
+}
+
+/**
+ * BasicInput interface
+ *
+ * @property {InputType} type - The type of the input
+ * @property {string} name - The key of the value of the element data or styles
+ * @property {string} [label] - The label of the input
+ * @property {ConfigsType} [configs] - Additional options for inputs that require more configuration
+ *
+ * @example
+ * // Bind to `element.data.title`
+ * name: "title"
+ * // Bind to `element.css["@desktop"].backgroundColor`
+ * name: "backgroundColor"
+ */
+export interface BasicInput<ConfigsType = AdditionalInputConfigs> {
+  type: InputType
+  name: string
+  label?: string
+  configs?: ConfigsType
+  condition?: string
+  defaultValue?: string | number | boolean | Partial<WeaverseImage> | { [x: string]: any }
+  placeholder?: string
+  helpText?: string
+}
+
+export interface SelectInputConfigs {
+  options?: { value: string; label: string }[]
+}
+
+export interface ToggleGroupConfigs {
+  options?: { value: string; label: string; icon?: string; weight?: string }[]
+}
+
+export interface RangeInputConfigs {
+  min?: number
+  max?: number
+  step?: number
+  unit?: string
+}
 
 declare global {
   interface Window {
