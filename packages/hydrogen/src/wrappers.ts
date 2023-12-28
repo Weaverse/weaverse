8a051aa7f8880100f6ee5f38cbed92b6f9ba9b18
hta218 <huynhtuananh21895@gmail.com>
2023-12-28 10:24:42 +0700

    Fix WeaverseItemStore instantiation in Weaverse class and update getSelectedProductOptions signature

--- packages/hydrogen/src/wrappers.ts
+++ packages/hydrogen/src/wrappers.ts
@@ -1,6 +1,9 @@
 import { getSelectedProductOptions as hydrogen_getSelectedProductOptions } from '@shopify/hydrogen'
+import type { SelectedOptionInput } from '@shopify/hydrogen/storefront-api-types'
 
-export function getSelectedProductOptions(request: Request) {
+export function getSelectedProductOptions(
+  request: Request,
+): SelectedOptionInput[] {
   let options = hydrogen_getSelectedProductOptions(request)
   return options.filter(
     ({ name }) => name !== 'isDesignMode' && !name.startsWith('weaverse'),
