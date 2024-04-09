1b27ca5f6b2241510bfb261637e957b1bd648bc7
hta218 <huynhtuananh21895@gmail.com>
2024-04-09 21:32:36 +0700

    Add 'url' to InputType in types.ts

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -45,6 +45,7 @@ export type InputType =
   | 'toggle-group'
   | 'swatches'
   | 'metaobject'
+  | 'url'
 
 // Complex types
 export type WeaverseCSSProperties = Stitches.CSS & Partial<Record<keyof typeof stitchesUtils, string | number>>
