b19969ce7d74ae324b291f1e1604fd4f7ffa3381
hta218 <huynhtuananh21895@gmail.com>
2024-04-23 14:51:13 +0700

    Add `resizeShopifyImage` function to utils.ts

--- packages/hydrogen/src/utils.ts
+++ packages/hydrogen/src/utils.ts
@@ -80,3 +80,28 @@ export function generateDataFromSchema({
   }
   return data
 }
+
+/**
+ * Resize an image to the specified dimensions.
+ *
+ * @param imageURL {string} URL of the image
+ * @param size {string} Shopify size attribute
+ * @returns {string} URL with size attribute
+ *
+ * @example
+ * resizeImage('https://cdn.shopify.com/image.jpg', '100x')
+ * resizeImage('https://cdn.shopify.com/image.jpg', '100x100')
+ */
+export function resizeShopifyImage(imageURL: string, size: string): string {
+  try {
+    if (size === 'original') {
+      return imageURL
+    } else {
+      let matches = imageURL.match(/(.*\/[\w-_.]+)\.(\w{2,4})/)
+      // @ts-ignore
+      return matches[1] + '_' + size + '.' + matches[2]
+    }
+  } catch (e) {
+    return imageURL
+  }
+}
